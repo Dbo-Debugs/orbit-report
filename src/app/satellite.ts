054d0ddf5ec749bc2a5b17764e19f22bd3815131
Dbo-Debugs <dsbowen87@gmail.com>
2020-06-04 16:42:27 -0400

    completed

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -8,8 +8,12 @@ export class Satellite {
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
-        this.launchDate = launchDate;
-        this.orbitType = orbitType;
         this.operational = operational;
+        this.orbitType = orbitType;
+        this.launchDate = launchDate;
+    }
+
+    shouldShowWarning(): boolean {
+        return this.type.toUpperCase() === "SPACE DEBRIS";
     }
 }
